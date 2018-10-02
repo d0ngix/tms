32343c58dd8282513336452dc12959ab5979f131
d0ngix <jmbrothers@yahoo.com>
2018-10-02 10:15:21 +0800

    Update teacher.js

--- api/controllers/teacher.js
+++ api/controllers/teacher.js
@@ -2,10 +2,10 @@
 'use strict';
 var mysql = require('mysql');
 const connection = mysql.createConnection({
-    host      : 'localhost',
-    user      : 'root',
-    password  : 'd0ngix777',
-    database  : 'tms'
+  host      : process.env.DB_HOST,
+  user      : process.env.DB_USER,
+  password  : process.env.DB_PASSWORD,
+  database  : process.env.DB_DATEBASE
 });
 
 var execQuery = (sql) => {
